3bcaa2bdb66e1ea4c84b9d831cd3bb2320531c77
raduflorescu <radu.florescu@totaljobsgroup.com>
2015-10-02 13:58:27 +0100

    Added paging for the Todays actions.

--- src/Kelpie.Web/Models/LogEntriesForDay.cs
+++ src/Kelpie.Web/Models/LogEntriesForDay.cs
@@ -7,44 +7,58 @@ using Kelpie.Core.Domain;
 
 namespace Kelpie.Web.Models
 {
-	public class LogEntriesForDay
-	{
-		public string DayOfWeek { get; set; }
-		public IEnumerable<LogEntry> LogEntries { get; set; }
+    public class LogEntriesForDay
+    {
+        public string DayOfWeek { get; set; }
 
-		public static LogEntriesForDay Create(IEnumerable<LogEntry> entries, DateTime dateTime)
-		{
-			string dayOfWeek = dateTime.ToString("dddd dd");
-			switch (dateTime.Day)
-			{
-				case 1:
-				case 21:
-				case 31:
-					dayOfWeek += "st";
-					break;
-				case 2:
-				case 22:
-					dayOfWeek += "nd";
-					break;
-				case 3:
-				case 23:
-					dayOfWeek += "rd";
-					break;
-				default:
-					dayOfWeek += "th";
-					break;
-			}
+        public IEnumerable<LogEntry> LogEntries { get; set; }
 
-			if (dateTime.Date == DateTime.Today.Date)
-				dayOfWeek = "Today";
-			else if (dateTime.Date == DateTime.Today.AddDays(-1).Date)
-				dayOfWeek = "Yesterday";
+        public int Page { get; set; }
 
-			return new LogEntriesForDay()
-			{
-				DayOfWeek = dayOfWeek,
-				LogEntries = entries.Where(x => x.DateTime.DayOfWeek == dateTime.DayOfWeek).Take(20)
-			};
-		}
-	}
+        public int Rows { get; set; }
+
+        public int TotalPages
+        {
+            get { return LogEntries.Count()/Rows + 1; }
+        }
+
+        public static LogEntriesForDay Create(IEnumerable<LogEntry> entries, DateTime dateTime, int page = 1, int rows = 100)
+        {
+            string dayOfWeek = dateTime.ToString("dddd dd");
+            switch (dateTime.Day)
+            {
+                case 1:
+                case 21:
+                case 31:
+                    dayOfWeek += "st";
+                    break;
+                case 2:
+                case 22:
+                    dayOfWeek += "nd";
+                    break;
+                case 3:
+                case 23:
+                    dayOfWeek += "rd";
+                    break;
+                default:
+                    dayOfWeek += "th";
+                    break;
+            }
+
+            if (dateTime.Date == DateTime.Today.Date)
+                dayOfWeek = "Today";
+            else if (dateTime.Date == DateTime.Today.AddDays(-1).Date)
+                dayOfWeek = "Yesterday";
+
+            return new LogEntriesForDay()
+            {
+                DayOfWeek = dayOfWeek,
+                LogEntries = entries.Where(x => x.DateTime.DayOfWeek == dateTime.DayOfWeek)
+                                .Skip((page - 1) * rows)
+                                .Take(rows),
+                Page = page,
+                Rows = rows
+            };
+        }
+    }
 }
